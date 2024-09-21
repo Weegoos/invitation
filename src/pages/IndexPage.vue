9bf89d072ccbd257dd5fba83fc58f4f03e0769a5
Weegoos <batyr.ashim05@mail.ru>
2024-09-21 13:27:20 +0500

    Invitation App: Checking the deploy

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,15 +1,14 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
-  </q-page>
+  <div>Batyr</div>
 </template>
 
 <script setup>
-defineOptions({
-  name: 'IndexPage'
+import { useQuasar } from "quasar";
+const name = "Batyr";
+sessionStorage.setItem("name", name);
+const $q = useQuasar();
+const sessionStorageName = sessionStorage.getItem("name");
+$q.notify({
+  message: sessionStorageName,
 });
 </script>
