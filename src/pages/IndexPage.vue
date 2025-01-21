76d1913217dcd8f7eb0da820b271a575f6840780
Weegoos <batyr.ashim05@mail.ru>
2025-01-21 23:32:38 +0500

    Finished

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -5,9 +5,16 @@
       style="width: 100vw; height: 100vh"
       spinner-color="primary"
       spinner-size="82px"
+      @load="onImageLoad"
     >
       <div class="absolute-center bg-transparent">
-        <q-btn no-caps color="secondary" label="Начать" @click="start" />
+        <q-btn
+          v-if="imageLoaded"
+          no-caps
+          color="secondary"
+          label="Начать"
+          @click="start"
+        />
       </div>
     </q-img>
     <DialogPage :openPage="openPage" />
@@ -18,6 +25,7 @@
 import { QSpinnerGears, useQuasar } from "quasar";
 import { onMounted, ref } from "vue";
 import DialogPage from "../components/Dialog/DialogPage.vue";
+
 const $q = useQuasar();
 
 const isSheAgreeSession = sessionStorage.getItem("agree");
@@ -30,7 +38,7 @@ const checkStatus = () => {
       message: "Добро пожаловать, Assem!",
       icon: "cloud_done",
       color: "positive",
-      timeout: 2500,
+      timeout: 5000,
     });
   }
 };
@@ -42,7 +50,13 @@ $q.notify({
 });
 
 const openPage = ref(false);
+const imageLoaded = ref(false); // Флаг для отслеживания загрузки изображения
+
 const start = () => {
   openPage.value = true;
+};
+
+const onImageLoad = () => {
+  imageLoaded.value = true; // Устанавливаем флаг в true после загрузки изображения
 };
 </script>
